bf6cac205cf159708e9e64c9a125742ac49f810a
Rima Paulionė <rimapaulione@gmail.com>
2026-04-25 12:42:38 +0300

    fix view issues

--- planner_ui/src/components/layout/AppLayout.tsx
+++ planner_ui/src/components/layout/AppLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from './Header';
 
 export function AppLayout() {
   return (
-    <div className="min-h-dvh bg-background">
+    <div className="min-h-dvh bg-background overflow-x-hidden">
       <Header />
       <main className="mx-auto max-w-5xl px-4 md:px-6 lg:px-8 py-6">
         <Outlet />
